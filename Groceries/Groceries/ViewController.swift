a5325f26ad0d50217ea047a64f708081d533e55e
Nicholas Isenhower <nicholasisenhower@Nicks-MacBook-Pro.local>
2017-06-10 21:03:31 -1000

    Initial commit

---
+++ Groceries/Groceries/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Groceries
+//
+//  Created by Nicholas Isenhower on 06/10/17.
+//  Copyright © 2017 Nicholas Isenhower. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    let GroceryList = ["apple", "orange", "watermelon"]
+    @IBOutlet var myTableView: UITableView!
+    
+    //making table view:
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return GroceryList.count
+    }
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let myCell: UITableViewCell = myTableView.dequeueReusableCell(withIdentifier: "prototype1", for: indexPath)
+        
+        myCell.textLabel?.text = GroceryList[indexPath.row]
+        myCell.imageView?.image = UIImage(named: GroceryList[indexPath.row])
+        
+        return myCell
+    }
+    
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        myTableView.dataSource = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
